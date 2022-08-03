d0679b121f4639ad497457850314a144daad0621
arnonr <arnon.r@tgde.kmutnb.ac.th>
2022-08-03 15:52:19 +0700

    Cart

--- controllers/Cart.controller.js
+++ controllers/Cart.controller.js
@@ -51,6 +51,7 @@ const methods = {
     try {
       const decoded = jwt.decode(req.headers.authorization.split(" ")[1]);
       // req.body.DeleteUserID = decoded.id;
+      console.log(req.body.AnimalID)
       await Service.delete(req.body.AnimalID, decoded.id);
       res.success("success", 204);
     } catch (error) {
