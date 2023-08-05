b68d919a21adbb87508fd45b815f4c86aa1f3dc9
arnonr <arnonr@kmutnb.ac.th>
2023-08-05 13:39:23 +0700

    db

--- configs/databases.js
+++ configs/databases.js
@@ -2,20 +2,36 @@ const config = require("../configs/app");
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  config.DbDatabase,
-  config.DbUsername,
-  config.DbPassword,
+  "AIDM_Final",
+  "cbg_aidm_db",
+  "Yjjbkiti1w0QAlYVWgPqruZdZ2jDSL",
+  //   config.DbDatabase ? config.DbDatabase : "aidm",
+  //   config.DbUsername ? config.DbUsername : "aidm",
+  //   config.DbPassword ? config.DbPassword : "2022@aidm",
   {
     logging: console.log,
-    host: config.DbHostname, //config.DbHostname ? config.DbHostname : "host.docker.internal",
+    host: "122.155.195.17", //config.DbHostname ? config.DbHostname : "host.docker.internal",
     port: "1433",
     dialect: "mssql",
+    // schema: "",
+    // operatorsAlias: false,
     dialectOptions: {
-        instanceName: "MSSQLSERVER",
+      useUTC: false,
+      instanceName: "MSSQLSERVER",
       options: {
         encrypt: false,
       },
+      //   requestTimeout: 600000,
+      //   authentication: {
+      //     type: "ntlm",
+      //     options: {
+      //       server: "http://122.155.195.17",
+      //       userName: "ARNON",
+      //       password: "21eba814IX795vqnrMi7M5aIBklxUZ",
+      //     },
+      //   },
     },
+    timezone: '+07:00',
     pool: {
       max: 5,
       min: 0,
