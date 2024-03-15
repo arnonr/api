a7ad09174780339de84b43c3055c8b9f1be8e5b8
arnonr <arnonr@kmutnb.ac.th>
2024-03-15 10:52:23 +0700

    update

--- controllers/Distribution.controller.js
+++ controllers/Distribution.controller.js
@@ -5,6 +5,7 @@ const methods = {
   async onGetAll(req, res) {
     try {
       let result = await Service.find(req);
+      
       res.success(result);
     } catch (error) {
       res.error(error);
@@ -35,7 +36,6 @@ const methods = {
     try {
       const decoded = jwt.decode(req.headers.authorization.split(" ")[1]);
       req.body.UpdatedUserID = decoded.id;
-
       const result = await Service.update(req.params.id, req.body);
       res.success(result);
     } catch (error) {
