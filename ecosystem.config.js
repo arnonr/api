d20370fd89679a92cab21a2d480f3dc0635d886d
arnonr <arnonr@kmutnb.ac.th>
2024-03-27 10:24:52 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,13 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "app.js",
-      instances: 32,
+      script: "index.js",
+      instances: "max",
       exec_mode: "cluster",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      node_args: "--optimize_for_size --max-old-space-size=920",
       load_balancing: {
         enabled: true,
         strategy: "round-robin",
