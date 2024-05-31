267abe774bad10448fb834d823e2fa7b3e13ba28
arnonr <arnonr@kmutnb.ac.th>
2024-05-31 15:46:38 +0700

    update progresscheckup

--- services/ProgressCheckup.service.js
+++ services/ProgressCheckup.service.js
@@ -1,157 +1,159 @@
 const config = require("../configs/app"),
-  { ErrorBadRequest, ErrorNotFound } = require("../configs/errorMethods"),
-  db = require("../models/ProgressCheckup"),
-  { Op, fn } = require("sequelize");
+    { ErrorBadRequest, ErrorNotFound } = require("../configs/errorMethods"),
+    db = require("../models/ProgressCheckup"),
+    { Op, fn } = require("sequelize");
 
 const Staff = require("../models/Staff");
 
 const methods = {
-  scopeSearch(req, limit, offset) {
-    // Where
-    $where = {};
+    scopeSearch(req, limit, offset) {
+        // Where
+        $where = {};
 
-    if (req.query.ProgressCheckupID)
-      $where["ProgressCheckupID"] = req.query.ProgressCheckupID;
+        if (req.query.ProgressCheckupID)
+            $where["ProgressCheckupID"] = req.query.ProgressCheckupID;
 
-    if (req.query.AnimalID) $where["AnimalID"] = req.query.AnimalID;
+        if (req.query.AnimalID) $where["AnimalID"] = req.query.AnimalID;
 
-    if (req.query.CheckupDate) $where["CheckupDate"] = req.query.CheckupDate;
-    if (req.query.BCSID) $where["BCSID"] = req.query.BCSID;
+        if (req.query.CheckupDate)
+            $where["CheckupDate"] = req.query.CheckupDate;
+        if (req.query.BCSID) $where["BCSID"] = req.query.BCSID;
 
-    if (req.query.ResponsibilityStaffID)
-      $where["ResponsibilityStaffID"] = req.query.ResponsibilityStaffID;
+        if (req.query.ResponsibilityStaffID)
+            $where["ResponsibilityStaffID"] = req.query.ResponsibilityStaffID;
 
-    if (req.query.isActive) $where["isActive"] = req.query.isActive;
-    if (req.query.CreatedUserID)
-      $where["CreatedUserID"] = req.query.CreatedUserID;
-    if (req.query.UpdatedUserID)
-      $where["UpdatedUserID"] = req.query.UpdatedUserID;
+        if (req.query.isActive) $where["isActive"] = req.query.isActive;
+        if (req.query.CreatedUserID)
+            $where["CreatedUserID"] = req.query.CreatedUserID;
+        if (req.query.UpdatedUserID)
+            $where["UpdatedUserID"] = req.query.UpdatedUserID;
 
-    $where["isRemove"] = 0;
-    const query = Object.keys($where).length > 0 ? { where: $where } : {};
+        $where["isRemove"] = 0;
+        const query = Object.keys($where).length > 0 ? { where: $where } : {};
 
-    // Order
-    $order = [["ProgressCheckupID", "ASC"]];
-    if (req.query.orderByField && req.query.orderBy)
-      $order = [
-        [
-          req.query.orderByField,
-          req.query.orderBy.toLowerCase() == "desc" ? "desc" : "asc",
-        ],
-      ];
-    query["order"] = $order;
+        // Order
+        $order = [["ProgressCheckupID", "ASC"]];
+        if (req.query.orderByField && req.query.orderBy)
+            $order = [
+                [
+                    req.query.orderByField,
+                    req.query.orderBy.toLowerCase() == "desc" ? "desc" : "asc",
+                ],
+            ];
+        query["order"] = $order;
 
-    if (!isNaN(limit)) query["limit"] = limit;
+        if (!isNaN(limit)) query["limit"] = limit;
 
-    if (!isNaN(offset)) query["offset"] = offset;
+        if (!isNaN(offset)) query["offset"] = offset;
 
-    query["include"] = [{ all: true, required: false }];
+        query["include"] = [{ all: true, required: false }];
 
-    return { query: query };
-  },
+        return { query: query };
+    },
 
-  find(req) {
-    const limit = +(req.query.size || config.pageLimit);
-    const offset = +(limit * ((req.query.page || 1) - 1));
-    const _q = methods.scopeSearch(req, limit, offset);
-    return new Promise(async (resolve, reject) => {
-      try {
-        Promise.all([
-          db.findAll(_q.query),
-          delete _q.query.include,
-          db.count(_q.query),
-        ])
-          .then((result) => {
-            const rows = result[0],
-              count = result[2];
-            resolve({
-              total: count,
-              lastPage: Math.ceil(count / limit),
-              currPage: +req.query.page || 1,
-              rows: rows,
-            });
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
-
-  findById(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const obj = await db.findByPk(id, {
-          include: { all: true, required: false },
+    find(req) {
+        const limit = +(req.query.size || config.pageLimit);
+        const offset = +(limit * ((req.query.page || 1) - 1));
+        const _q = methods.scopeSearch(req, limit, offset);
+        return new Promise(async (resolve, reject) => {
+            try {
+                Promise.all([
+                    db.findAll(_q.query),
+                    delete _q.query.include,
+                    db.count(_q.query),
+                ])
+                    .then((result) => {
+                        const rows = result[0],
+                            count = result[2];
+                        resolve({
+                            total: count,
+                            lastPage: Math.ceil(count / limit),
+                            currPage: +req.query.page || 1,
+                            rows: rows,
+                        });
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            } catch (error) {
+                reject(error);
+            }
         });
+    },
 
-        if (!obj) reject(ErrorNotFound("id: not found"));
-        resolve(obj.toJSON());
-      } catch (error) {
-        reject(ErrorNotFound("id: not found"));
-      }
-    });
-  },
+    findById(id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const obj = await db.findByPk(id, {
+                    include: { all: true, required: false },
+                });
 
-  insert(data) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        //check เงื่อนไขตรงนี้ได้
-        data.createdAt = fn("GETDATE");
+                if (!obj) reject(ErrorNotFound("id: not found"));
+                resolve(obj.toJSON());
+            } catch (error) {
+                reject(ErrorNotFound("id: not found"));
+            }
+        });
+    },
 
-        const obj = new db(data);
-        const inserted = await obj.save();
+    insert(data) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //check เงื่อนไขตรงนี้ได้
+                data.createdAt = fn("GETDATE");
 
-        let res = methods.findById(inserted.ProgressCheckupID);
+                const obj = new db(data);
+                const inserted = await obj.save();
 
-        resolve(res);
-      } catch (error) {
-        reject(ErrorBadRequest(error.message));
-      }
-    });
-  },
+                let res = methods.findById(inserted.ProgressCheckupID);
 
-  update(id, data) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        // Check ID
-        const obj = await db.findByPk(id);
-        if (!obj) reject(ErrorNotFound("id: not found"));
+                resolve(res);
+            } catch (error) {
+                reject(ErrorBadRequest(error.message));
+            }
+        });
+    },
 
-        // Update
-        data.ProgressCheckupID = parseInt(id);
+    update(id, data) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                // Check ID
+                const obj = await db.findByPk(id);
+                if (!obj) reject(ErrorNotFound("id: not found"));
 
-        data.updatedAt = fn("GETDATE");
+                // Update
+                data.ProgressCheckupID = parseInt(id);
 
-        await db.update(data, { where: { ProgressCheckupID: id } });
+                data.updatedAt = fn("GETDATE");
 
-        let res = methods.findById(data.ProgressCheckupID);
+                data.createdAt = undefined;
+                await db.update(data, { where: { ProgressCheckupID: id } });
 
-        resolve(res);
-      } catch (error) {
-        reject(ErrorBadRequest(error.message));
-      }
-    });
-  },
+                let res = methods.findById(data.ProgressCheckupID);
 
-  delete(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const obj = await db.findByPk(id);
-        if (!obj) reject(ErrorNotFound("id: not found"));
+                resolve(res);
+            } catch (error) {
+                reject(ErrorBadRequest(error.message));
+            }
+        });
+    },
 
-        await db.update(
-          { isRemove: 1, isActive: 0, updatedAt: fn("GETDATE") },
-          { where: { ProgressCheckupID: id } }
-        );
-        resolve();
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
+    delete(id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const obj = await db.findByPk(id);
+                if (!obj) reject(ErrorNotFound("id: not found"));
+
+                await db.update(
+                    { isRemove: 1, isActive: 0, updatedAt: fn("GETDATE") },
+                    { where: { ProgressCheckupID: id } }
+                );
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        });
+    },
 };
 
 module.exports = { ...methods };
