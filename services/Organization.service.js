7136565523832d51e6a4e3fd6c237ca48e2859ca
arnonr <arnonr@kmutnb.ac.th>
2025-02-05 21:53:26 +0700

    Update

--- services/Organization.service.js
+++ services/Organization.service.js
@@ -1,23 +1,23 @@
 const config = require("../configs/app"),
-  { ErrorBadRequest, ErrorNotFound } = require("../configs/errorMethods"),
-  db = require("../models/Organization"),
-  Province = require("../models/Province"),
-  { Op, fn } = require("sequelize");
+    { ErrorBadRequest, ErrorNotFound } = require("../configs/errorMethods"),
+    db = require("../models/Organization"),
+    Province = require("../models/Province"),
+    { Op, fn } = require("sequelize");
 
 const Sequelize = require("sequelize"),
-  { sequelize } = require("../configs/databases");
+    { sequelize } = require("../configs/databases");
 
 const methods = {
-  async scopeSearch(req, limit, offset) {
-    // Where
-    $where = {};
+    async scopeSearch(req, limit, offset) {
+        // Where
+        $where = {};
 
-    if (req.query.OrganizationID)
-      $where["OrganizationID"] = req.query.OrganizationID;
+        if (req.query.OrganizationID)
+            $where["OrganizationID"] = req.query.OrganizationID;
 
-    if (req.query.ParentOrganizationID) {
-      // $where["ParentOrganizationID"] = req.query.ParentOrganizationID;
-      let organization = `with recursive cte (OrganizationID, ParentOrganizationID) as (
+        if (req.query.ParentOrganizationID) {
+            // $where["ParentOrganizationID"] = req.query.ParentOrganizationID;
+            let organization = `with recursive cte (OrganizationID, ParentOrganizationID) as (
         select     OrganizationID,
                    ParentOrganizationID
         from       Organization
@@ -31,276 +31,310 @@ const methods = {
       )
       select * from cte;`;
 
-      const res = await sequelize.query(organization);
+            const res = await sequelize.query(organization);
 
-      let orgArr = [req.query.ParentOrganizationID];
-      res[0].map((r) => {
-        orgArr.push(r.OrganizationID);
-      });
+            let orgArr = [req.query.ParentOrganizationID];
+            res[0].map((r) => {
+                orgArr.push(r.OrganizationID);
+            });
 
-      // $where["OrganizationID"] = { [Op.in]: orgArr };
-      $where["OrganizationID"] = {
-        [Op.and]: [{ [Op.in]: orgArr }, { [Op.not]: 2 }],
-      };
-    }
-
-    if (req.query.OrganizationCode)
-      $where["OrganizationCode"] = {
-        [Op.like]: "%" + req.query.OrganizationCode + "%",
-      };
-
-    if (req.query.OrganizationName)
-      $where["OrganizationName"] = {
-        [Op.like]: "%" + req.query.OrganizationName + "%",
-      };
-
-    if (req.query.OrganizationExecutive)
-      $where["OrganizationExecutive"] = {
-        [Op.like]: "%" + req.query.OrganizationExecutive + "%",
-      };
-
-    // if (req.query.OrganizationExecutivePosition)
-    //   $where["OrganizationExecutivePosition"] = {
-    //     [Op.like]: "%" + req.query.OrganizationExecutivePosition + "%",
-    //   };
-
-    if (req.query.OrganizationTypeID)
-      $where["OrganizationTypeID"] = req.query.OrganizationTypeID;
-
-    // if (req.query.OrganizationZoneID)
-    //   $where["OrganizationZoneID"] = req.query.OrganizationZoneID;
-
-    if (req.query.OrganizationAiZoneID) {
-      let province1 = await Province.findAll({
-        where: {
-          AiZoneID: req.query.OrganizationAiZoneID,
-        },
-      });
-
-      let province_id_arr = province1.map((x) => {
-        return x.ProvinceID;
-      });
-
-      $where["OrganizationProvinceID"] = {
-        [Op.in]: province_id_arr,
-      };
-    }
-
-    if (req.query.OrganizationZoneID) {
-      let province = await Province.findAll({
-        where: {
-          OrganizationZoneID: req.query.OrganizationZoneID,
-        },
-      });
-
-      let province_id_arr = province.map((x) => {
-        return x.ProvinceID;
-      });
-
-      $where["OrganizationProvinceID"] = {
-        [Op.in]: province_id_arr,
-      };
-    }
-
-    if (req.query.ProvinceID)
-      $where["OrganizationProvinceID"] = req.query.ProvinceID;
-
-    if (req.query.AmphurID) $where["OrganizationAmphurID"] = req.query.AmphurID;
-
-    if (req.query.TumbolID) $where["OrganizationTumbolID"] = req.query.TumbolID;
-
-    if (req.query.OrganizationZipCode)
-      $where["OrganizationZipCode"] = req.query.OrganizationZipCode;
-
-    if (req.query.isActive) $where["isActive"] = req.query.isActive;
-    if (req.query.CreatedUserID)
-      $where["CreatedUserID"] = req.query.CreatedUserID;
-    if (req.query.UpdatedUserID)
-      $where["UpdatedUserID"] = req.query.UpdatedUserID;
-
-    $where["isRemove"] = 0;
-    const query = Object.keys($where).length > 0 ? { where: $where } : {};
-
-    // Order
-    $order = [["OrganizationID", "ASC"]];
-    if (req.query.orderByField && req.query.orderBy)
-      $order = [
-        [
-          req.query.orderByField,
-          req.query.orderBy.toLowerCase() == "desc" ? "desc" : "asc",
-        ],
-      ];
-    query["order"] = $order;
-
-    if (!isNaN(limit)) query["limit"] = limit;
-
-    if (!isNaN(offset)) query["offset"] = offset;
-
-    let include = [];
-
-    if (req.query.includeAll) {
-      if (req.query.includeAll == "false") {
-        if (req.query.includeProvince == "true") {
-          include.push({
-            association: "Province",
-            required: false,
-          });
+            // $where["OrganizationID"] = { [Op.in]: orgArr };
+            $where["OrganizationID"] = {
+                [Op.and]: [{ [Op.in]: orgArr }, { [Op.not]: 2 }],
+            };
         }
-      } else {
-        include.unshift({ all: true, required: false });
-      }
-    } else {
-      include.unshift({ all: true, required: false });
-    }
+      
 
-    query["include"] = include;
+        if (req.query.OrganizationCode)
+            $where["OrganizationCode"] = {
+                [Op.like]: "%" + req.query.OrganizationCode + "%",
+            };
 
-    return { query: query };
-  },
+        if (req.query.OrganizationName)
+            $where["OrganizationName"] = {
+                [Op.like]: "%" + req.query.OrganizationName + "%",
+            };
 
-  async find(req) {
-    const limit = +(req.query.size || config.pageLimit);
-    const offset = +(limit * ((req.query.page || 1) - 1));
-    const _q = await methods.scopeSearch(req, limit, offset);
-    return new Promise(async (resolve, reject) => {
-      try {
-        Promise.all([
-          db.findAll(_q.query),
-          delete _q.query.include,
-          db.count(_q.query),
-        ])
-          .then((result) => {
-            const rows = result[0],
-              count = result[2];
-            resolve({
-              total: count,
-              lastPage: Math.ceil(count / limit),
-              currPage: req.query.page ? +req.query.page : 1,
-              rows: rows,
+        if (req.query.OrganizationExecutive)
+            $where["OrganizationExecutive"] = {
+                [Op.like]: "%" + req.query.OrganizationExecutive + "%",
+            };
+
+        // if (req.query.OrganizationExecutivePosition)
+        //   $where["OrganizationExecutivePosition"] = {
+        //     [Op.like]: "%" + req.query.OrganizationExecutivePosition + "%",
+        //   };
+
+        if (req.query.OrganizationTypeID)
+            $where["OrganizationTypeID"] = req.query.OrganizationTypeID;
+
+        // if (req.query.OrganizationZoneID)
+        //   $where["OrganizationZoneID"] = req.query.OrganizationZoneID;
+     
+        if (req.query.OrganizationAiZoneID) {
+            let province1 = await Province.findAll({
+                where: {
+                    AiZoneID: req.query.OrganizationAiZoneID,
+                },
             });
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
 
-  findById(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const obj = await db.findByPk(id, {
-          include: { all: true, required: false },
+            let province_id_arr = province1.map((x) => {
+                return x.ProvinceID;
+            });
+
+            $where["OrganizationProvinceID"] = {
+                [Op.in]: province_id_arr,
+            };
+        }
+
+        if (req.query.OrganizationZoneID) {
+            console.log(100);
+            console.log($where);
+
+            let province_5 = await Province.findAll({
+                where: {
+                    OrganizationZoneID: Number(req.query.OrganizationZoneID),
+                },
+            });
+            console.log(
+                "✅ Province Query Result:",
+                province_5.map((p) => p.ProvinceID)
+            );
+            console.log(106);
+            console.log($where);
+
+            let province_id_arr = province_5.map((x) => {
+                return x.ProvinceID;
+            });
+            console.log(
+                "🔍 Before updating OrganizationProvinceID - $where:",
+                JSON.stringify($where, null, 2)
+            );
+
+            console.log(107);
+            console.log($where);
+
+            $where["OrganizationProvinceID"] = {
+                [Op.in]: province_id_arr,
+            };
+            console.log(
+                "✅ After updating OrganizationProvinceID - $where:",
+                JSON.stringify($where, null, 2)
+            );
+
+            console.log(112);
+            console.log($where);
+        }
+
+        if (req.query.OrganizationProvinceID)
+            $where["OrganizationProvinceID"] = req.query.OrganizationProvinceID;
+
+        if (req.query.OrganizationAmphurID)
+            $where["OrganizationAmphurID"] = req.query.OrganizationAmphurID;
+
+        if (req.query.OrganizationTumbolID)
+            $where["OrganizationTumbolID"] = req.query.OrganizationTumbolID;
+
+        if (req.query.OrganizationZipCode)
+            $where["OrganizationZipCode"] = req.query.OrganizationZipCode;
+
+        if (req.query.isActive) $where["isActive"] = req.query.isActive;
+        if (req.query.CreatedUserID)
+            $where["CreatedUserID"] = req.query.CreatedUserID;
+        if (req.query.UpdatedUserID)
+            $where["UpdatedUserID"] = req.query.UpdatedUserID;
+
+
+        if ($where.hasOwnProperty("ProvinceID")) {
+            delete $where["ProvinceID"];
+        }
+        $where["isRemove"] = 0;
+        const query = Object.keys($where).length > 0 ? { where: $where } : {};
+
+
+        // Order
+        $order = [["OrganizationID", "ASC"]];
+        if (req.query.orderByField && req.query.orderBy)
+            $order = [
+                [
+                    req.query.orderByField,
+                    req.query.orderBy.toLowerCase() == "desc" ? "desc" : "asc",
+                ],
+            ];
+        query["order"] = $order;
+
+        if (!isNaN(limit)) query["limit"] = limit;
+
+        if (!isNaN(offset)) query["offset"] = offset;
+
+        let include = [];
+
+        if (req.query.includeAll) {
+            if (req.query.includeAll == "false") {
+                if (req.query.includeProvince == "true") {
+                    include.push({
+                        association: "Province",
+                        required: false,
+                    });
+                }
+            } else {
+                include.unshift({ all: true, required: false });
+            }
+        } else {
+            include.unshift({ all: true, required: false });
+        }
+
+        query["include"] = include;
+
+        return { query: query };
+    },
+
+    async find(req) {
+        const limit = +(req.query.size || config.pageLimit);
+        const offset = +(limit * ((req.query.page || 1) - 1));
+        const _q = await methods.scopeSearch(req, limit, offset);
+        return new Promise(async (resolve, reject) => {
+            try {
+                Promise.all([
+                    db.findAll(_q.query),
+                    delete _q.query.include,
+                    db.count(_q.query),
+                ])
+                    .then((result) => {
+                        const rows = result[0],
+                            count = result[2];
+                        resolve({
+                            total: count,
+                            lastPage: Math.ceil(count / limit),
+                            currPage: req.query.page ? +req.query.page : 1,
+                            rows: rows,
+                        });
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            } catch (error) {
+                reject(error);
+            }
         });
+    },
 
-        if (!obj) reject(ErrorNotFound("id: not found"));
-        resolve(obj.toJSON());
-      } catch (error) {
-        reject(ErrorNotFound("id: not found"));
-      }
-    });
-  },
+    findById(id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const obj = await db.findByPk(id, {
+                    include: { all: true, required: false },
+                });
 
-  insert(data) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        //check เงื่อนไขตรงนี้ได้
-        data.createdAt = fn("GETDATE");
+                if (!obj) reject(ErrorNotFound("id: not found"));
+                resolve(obj.toJSON());
+            } catch (error) {
+                reject(ErrorNotFound("id: not found"));
+            }
+        });
+    },
 
-        const obj = new db(data);
-        const inserted = await obj.save();
+    insert(data) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //check เงื่อนไขตรงนี้ได้
+                data.createdAt = fn("GETDATE");
 
-        let res = methods.findById(inserted.OrganizationID);
+                const obj = new db(data);
+                const inserted = await obj.save();
 
-        resolve(res);
-      } catch (error) {
-        reject(ErrorBadRequest(error.message));
-      }
-    });
-  },
+                let res = methods.findById(inserted.OrganizationID);
 
-  update(id, data) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        // Check ID
-        const obj = await db.findByPk(id);
-        if (!obj) reject(ErrorNotFound("id: not found"));
+                resolve(res);
+            } catch (error) {
+                reject(ErrorBadRequest(error.message));
+            }
+        });
+    },
 
-        // Update
-        data.OrganizationID = parseInt(id);
+    update(id, data) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                // Check ID
+                const obj = await db.findByPk(id);
+                if (!obj) reject(ErrorNotFound("id: not found"));
 
-        data.updatedAt = fn("GETDATE");
+                // Update
+                data.OrganizationID = parseInt(id);
 
-        await db.update(data, { where: { OrganizationID: id } });
+                data.updatedAt = fn("GETDATE");
 
-        let res = methods.findById(data.OrganizationID);
+                await db.update(data, { where: { OrganizationID: id } });
 
-        resolve(res);
-      } catch (error) {
-        reject(ErrorBadRequest(error.message));
-      }
-    });
-  },
+                let res = methods.findById(data.OrganizationID);
 
-  delete(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const obj = await db.findByPk(id);
-        if (!obj) reject(ErrorNotFound("id: not found"));
+                resolve(res);
+            } catch (error) {
+                reject(ErrorBadRequest(error.message));
+            }
+        });
+    },
 
-        await db.update(
-          { isRemove: 1, isActive: 0, updatedAt: fn("GETDATE") },
-          { where: { OrganizationID: id } }
-        );
-        resolve();
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
+    delete(id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const obj = await db.findByPk(id);
+                if (!obj) reject(ErrorNotFound("id: not found"));
 
-  async selection(req) {
-    const limit = +(req.query.size || config.pageLimit);
-    const offset = +(limit * ((req.query.page || 1) - 1));
-    const _q = await methods.scopeSearch(req, limit, offset);
+                await db.update(
+                    { isRemove: 1, isActive: 0, updatedAt: fn("GETDATE") },
+                    { where: { OrganizationID: id } }
+                );
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        });
+    },
 
-    return new Promise(async (resolve, reject) => {
-      try {
-        Promise.all([db.findAll({ ..._q.query, limit: limit, offset: offset })])
-          .then(async (result) => {
-            let rows = result[0];
+    async selection(req) {
+        const limit = +(req.query.size || config.pageLimit);
+        const offset = +(limit * ((req.query.page || 1) - 1));
+        const _q = await methods.scopeSearch(req, limit, offset);
 
-            rows = rows.map((data) => {
-              let d = {
-                OrganizationID: data.OrganizationID,
-                ParentOrganizationID: data.ParentOrganizationID,
-                OrganizationCode: data.OrganizationCode,
-                OrganizationName: data.OrganizationName,
-                ProvinceName: data.Province?.ProvinceName,
-                OrganizationTypeID: data.OrganizationTypeID,
-                OrganizationZoneID: data.OrganizationZoneID,
-                OrganizationProvinceID: data.OrganizationProvinceID,
-                OrganizationAmphurID: data.OrganizationAmphurID,
-                OrganizationTumbolID: data.OrganizationTumbolID,
-                OrganizationAiZoneID: data.OrganizationAiZoneID,
-              };
-              return d;
-            });
+        return new Promise(async (resolve, reject) => {
+            try {
+                Promise.all([
+                    db.findAll({ ..._q.query, limit: limit, offset: offset }),
+                ])
+                    .then(async (result) => {
+                        let rows = result[0];
 
-            resolve({
-              rows: rows,
-            });
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
+                        rows = rows.map((data) => {
+                            let d = {
+                                OrganizationID: data.OrganizationID,
+                                ParentOrganizationID: data.ParentOrganizationID,
+                                OrganizationCode: data.OrganizationCode,
+                                OrganizationName: data.OrganizationName,
+                                ProvinceName: data.Province?.ProvinceName,
+                                OrganizationTypeID: data.OrganizationTypeID,
+                                OrganizationZoneID: data.OrganizationZoneID,
+                                OrganizationProvinceID:
+                                    data.OrganizationProvinceID,
+                                OrganizationAmphurID: data.OrganizationAmphurID,
+                                OrganizationTumbolID: data.OrganizationTumbolID,
+                                OrganizationAiZoneID: data.OrganizationAiZoneID,
+                            };
+                            return d;
+                        });
+
+                        resolve({
+                            rows: rows,
+                        });
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    },
 };
 
 module.exports = { ...methods };
