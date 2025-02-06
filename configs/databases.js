6f2d91fa0f0f828725f9f2decd57ce3398d39cac
arnonr <arnonr@kmutnb.ac.th>
2025-02-06 13:34:25 +0700

    DB

--- configs/databases.js
+++ configs/databases.js
@@ -2,60 +2,60 @@ const config = require("../configs/app");
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "AIDM_Final",
-  "cbg_aidm_db",
-  process.env.DB_PASSWORD,
-  
-//   "Yjjbkiti1w0QAlYVWgPqruZdZ2jDSL",
-  //   config.DbDatabase ? config.DbDatabase : "aidm",
-  //   config.DbUsername ? config.DbUsername : "aidm",
-  //   config.DbPassword ? config.DbPassword : "2022@aidm",
-  {
-    logging: console.log,
-    host: "122.155.195.17", //config.DbHostname ? config.DbHostname : "host.docker.internal",
-    port: "1433",
-    dialect: "mssql",
-    // schema: "",
-    // operatorsAlias: false,
-    dialectOptions: {
-      useUTC: false,
-      instanceName: "MSSQLSERVER",
-      options: {
-        encrypt: false,
-        requestTimeout: 1000000000,
-      },
-    //   options: { "requestTimeout": 300000 }
-      //   requestTimeout: 600000,
-      //   authentication: {
-      //     type: "ntlm",
-      //     options: {
-      //       server: "http://122.155.195.17",
-      //       userName: "ARNON",
-      //       password: "21eba814IX795vqnrMi7M5aIBklxUZ",
-      //     },
-      //   },
-    },
-    timezone: "+07:00",
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 60000, 
-      idle: 10000,
-    },
-    define: {
-      timestamps: false,
-    },
-  }
+    "AIDM_Final",
+    "cbg_aidm_db",
+    process.env.DB_PASSWORD,
+
+    //   "Yjjbkiti1w0QAlYVWgPqruZdZ2jDSL",
+    //   config.DbDatabase ? config.DbDatabase : "aidm",
+    //   config.DbUsername ? config.DbUsername : "aidm",
+    //   config.DbPassword ? config.DbPassword : "2022@aidm",
+    {
+        logging: console.log,
+        host: "122.155.195.17", //config.DbHostname ? config.DbHostname : "host.docker.internal",
+        port: "1433",
+        dialect: "mssql",
+        // schema: "",
+        // operatorsAlias: false,
+        dialectOptions: {
+            useUTC: false,
+            instanceName: "MSSQLSERVER",
+            options: {
+                encrypt: false,
+                requestTimeout: 1000000000,
+            },
+            //   options: { "requestTimeout": 300000 }
+            //   requestTimeout: 600000,
+            //   authentication: {
+            //     type: "ntlm",
+            //     options: {
+            //       server: "http://122.155.195.17",
+            //       userName: "ARNON",
+            //       password: "21eba814IX795vqnrMi7M5aIBklxUZ",
+            //     },
+            //   },
+        },
+        timezone: "+07:00",
+        pool: {
+            max: 30,
+            min: 0,
+            acquire: 60000,
+            idle: 10000,
+        },
+        define: {
+            timestamps: false,
+        },
+    }
 );
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("connected..");
-  })
-  .catch((err) => {
-    console.log("Error" + err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log("connected..");
+    })
+    .catch((err) => {
+        console.log("Error" + err);
+    });
 
 const db = {};
 
